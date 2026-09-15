6d579b8e4774e1a0ca523510a146323aacdb2c31
goldenBoy16 <140768793+goldenBoy16@users.noreply.github.com>
2026-09-15 19:48:14 -0400

    estrategias

--- parcial2/solucion.php
+++ parcial2/solucion.php
@@ -9,5 +9,29 @@ interface MultaStrategyInterface {
     public function calcularMulta(int $diasAtraso): float;
 }
 
+// 2. tengo las estrategias
+
+// Estrategia 1: Socio Infantil (no paga, solo bloquea)
+class MultaInfantilStrategy implements MultaStrategyInterface {
+    public function calcularMulta(int $diasAtraso): float {
+        return 0.0;
+    }
+}
+
+// Estrategia 2: Socio Adulto (2 Bs por día)
+class MultaAdultoStrategy implements MultaStrategyInterface {
+    public function calcularMulta(int $diasAtraso): float {
+        return $diasAtraso * 2.0;
+    }
+}
+
+// Estrategia 3: Socio Tercera Edad (1 Bs por día con tope de 20 Bs)
+class MultaTerceraEdadStrategy implements MultaStrategyInterface {
+    public function calcularMulta(int $diasAtraso): float {
+        $calculo = $diasAtraso * 1.0;
+        return min($calculo, 20.0);
+    }
+}
+
 
 
